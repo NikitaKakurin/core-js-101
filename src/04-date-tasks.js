ef65ba956f0ac2a49ab175d54991bc7e7f54db3f
NikitaKakurin <neyasut87@rambler.ru>
2022-07-23 22:57:59 +0300

    feat: add all Data

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(value) {
+  return new Date(value);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(value) {
+  return new Date(value);
 }
 
 
@@ -53,8 +53,12 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(date) {
+  const year = +date.getFullYear();
+  if (year % 4) return false;
+  if (year % 100) return true;
+  if (year % 400) return false;
+  return true;
 }
 
 
@@ -73,8 +77,27 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  function getNextTimeAndCurrent(time, divider) {
+    const value = time % divider;
+    const timeInNext = Math.floor(time / divider);
+    return [value, timeInNext];
+  }
+
+  function fixValue(value, length) {
+    let valueStr = value.toString();
+    while (valueStr.length < length) {
+      valueStr = `0${valueStr}`;
+    }
+    return valueStr;
+  }
+
+  const timespan = endDate - startDate;
+  const [sss, timeInSec] = getNextTimeAndCurrent(timespan, 1000);
+  const [ss, timeInMin] = getNextTimeAndCurrent(timeInSec, 60);
+  const [mm, hh] = getNextTimeAndCurrent(timeInMin, 60);
+
+  return `${fixValue(hh, 2)}:${fixValue(mm, 2)}:${fixValue(ss, 2)}.${fixValue(sss, 3)}`;
 }
 
 
@@ -94,8 +117,16 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(date) {
+  const mm = date.getUTCMinutes();
+  const hours = date.getUTCHours();
+  const hh = hours > 12 ? hours - 12 : hours;
+  const angleForMinute = 360 / 60;
+  const angleForHour = 360 / 12;
+  const angleMm = angleForMinute * mm;
+  const angleHh = angleForHour * hh + angleForHour * (mm / 60);
+  const angle = Math.abs(angleHh - angleMm > 180 ? angleHh - angleMm - 180 : angleHh - angleMm);
+  return angle * (Math.PI / 180);
 }
 
 
